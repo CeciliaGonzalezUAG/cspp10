5cb989f5363623a3b400dadf66f886a9b2accc4b
Cecilia Gonzalez <ceciliagonzalez@uagateway.org>
2016-10-06 16:12:43 +0000

    Worked on Unit 2 Lab

--- unit2/FILN_simplecalc.py
+++ unit2/FILN_simplecalc.py
@@ -1,8 +1,15 @@
 equation=input("Enter and equation:")
-operation=float(equation[1])
+operation=equation[1]
 number1=float(equation[0])
 number2=float(equation[2])
-multiply=number1*number2
 
-if operation=="*":
-    print("The result is "+multiply)
+if operation =="*":
+    print("The result is " + str(number1*number2));
+elif operation =="+":
+    print("The result is " + str(number1+number2));
+elif operation=="-":
+    print("The result is " + str(number1-number2))
+elif operation=="/":
+    print("The result is " + str(number1/number2));
+elif operation=="%":
+    print("The result is " + str(number1%number2))

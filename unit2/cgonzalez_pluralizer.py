246e2f3a26b2571dd302b1d3b8108af8c8150aa0
Cecilia Gonzalez <ceciliagonzalez@uagateway.org>
2016-10-13 19:01:19 +0000

    Unit 2 labs

--- unit2/cgonzalez_pluralizer.py
+++ unit2/cgonzalez_pluralizer.py
@@ -5,24 +5,27 @@ y="ies"
 sh="es"
 ch="es"
 us="i"
-twoy="s"
-if word[-2:]=="fe":
+s="s"
+
+if number=="1":
+    print(number+" "+word)
+elif word[-2:]=="fe":
     print(number +" "+ word[0:-2]+fe)
-elif word[-1:]=="ies":
-    print(number +" "+ word[0:-2]+ies)
+elif word[-2:]=="oy":
+    print(number +" "+ word+s)
 elif word[-2:]=="sh":
     print(number +" "+ word[0:-2]+sh)
 elif word[-2:]=="ch":
     print(number +" "+ word[0:-2]+ch)
-if word[-2:]=="us":
+elif word[-2:]=="us":
     print(number +" "+ word[0:-2]+ch)
-if word[-2:]=="ay":
-    print(number +" "+ word[0:-2]+twoy)
-if word[-2:]=="oy":
-    print(number +" "+ word[0:-2]+twoy)
-if word[-2:]=="ey":
-    print(number +" "+ word[0:-2]+twoy)
-if word[-2:]=="uy":
-    print(number +" "+ word[0:-2]+twoy)
+elif word[-2:]=="ay":
+    print(number +" "+ word+s)
+elif word[-1:]=="y":
+    print(number +" "+ word[0:-1]+y)
+elif word[-2:]=="ey":
+    print(number +" "+ word+s)
+elif word[-2:]=="uy":
+    print(number +" "+ word+s)
 else:
     print(number +" "+ word+"s")

fe71d8088f1ddad5623293bd9da7a42cefe69aab
Cecilia Gonzalez <ceciliagonzalez@uagateway.org>
2016-11-22 17:15:18 +0000

    Working on Lab 4-1

--- unit4/cgonzalez_rps.py
+++ unit4/cgonzalez_rps.py
@@ -3,9 +3,15 @@ import random
 #   arguments: none
 #   purpose: present player with options, use input() to get player move
 #   returns: the player's move as either 'r', 'p', or 's'
-def get_p1_move()
-    #code here
-
+def get_p1_move():
+    #code 
+    person=input("1 = Paper \n2 = Rock \n3 = Scissors \n PLease Enter a number from 1-3: ")
+    if person == '1':
+        return 'p'
+    elif person == '2':
+        return 'r'
+    elif person == '3':
+        return 's'
 #function name: get_comp_move():
 #   arguments: none
 #   purpose: randomly generates the computer's move,
@@ -13,14 +19,16 @@ def get_p1_move()
 #   returns: the computer's randomly generated move
 def get_comp_move(): 
     #code here
-
+    computer=random.randint(1,3)
+    return computer
 #function name: get_rounds
 #   arguments: none
 #   purpose: allows the user to choose a number of rounds from 1 to 9.
 #   returns: the user-chosen number of rounds
 def get_rounds():
     #code here
-
+    rounds= input("How many rounds from 1-9?: ")
+    return int(rounds)
 #function name: get_round_winner
 #   arguments: player move, computer move
 #   purpose: based on the player and computer's move, determine
@@ -31,6 +39,33 @@ def get_rounds():
 #               "tie" if it's a tie
 def get_round_winner(p1move, cmove):
     #code here
+    if p1move=="r" and cmove=="p":
+        print("The computer won")
+        return "comp"
+    elif p1move=="p" and cmove=="s":
+        print("The computer won")
+        return "comp"
+    elif p1move=="s" and cmove=="r":
+        print("The computer won")
+        return "comp"
+    elif cmove=="rock" and p1move=="paper":
+        print("The player won")
+        return "player"
+    elif cmove=="p" and p1move=="s":
+        print("The player won")
+        return "player"
+    elif cmove=="s" and p1move=="r":
+        print("The player won")
+        return "player"
+    elif cmove=="r" and p1move=="r":
+        print("tie")
+        return "tie"
+    elif cmove=="p" and p1move=="p":
+        print("Tie")
+        return "tie"
+    elif cmove=="s" and p1move=="s":
+        print("tie")
+        return "tie"
 
 #function name: get_full_move
 #   arguments: a single letter move 'r','p', or 's'
@@ -41,13 +76,18 @@ def get_round_winner(p1move, cmove):
 #               "Scissors" if given "s"
 def get_full_move(shortmove):
     #code here
-
+    if shortmove == "r":
+        return "Rock"
+    elif shortmove == "s":
+        return "Scissors"
+    elif shortmove == "p":
+        return "Paper"
 #function name: print_score
 #   arguments: player score, computer score, number of ties
 #   purpose: prints the scoreboard
 #   returns: none
 def print_score(pscore, cscore, ties):
-    #code here
+    print ("Player 1 has {} points\nComputer has {} points\nYou both tied {} times".format(pscore,cscore,ties))
 
 #function name: rps
 #   arguments: none
@@ -55,14 +95,20 @@ def print_score(pscore, cscore, ties):
 #               all the other functions to create RPS
 #   returns: none
 def rps():
-    #code here
-
-#function name: tests
-#   arguments: none
-#   purpose: a place for you to write your tests.  replace 'rps' below
-#               with 'tests' to run this function instead of the game loop
-#   returns: none
-def test():
-    #code here
-
+    score_player = 0
+    score_comp = 0
+    ties = 0
+    rounds_no = get_rounds()
+    for c in range(rounds_no):
+        player_move = get_p1_move()
+        comp_move = get_comp_move()
+        round_winner = get_round_winner(player_move,comp_move)
+        print(round_winner)
+        if round_winner == "player":
+            score_player = score_player + 1
+        elif round_winner == "comp":
+            score_comp = score_comp + 1
+        else:
+            ties = ties + 1
+        print_score(score_player, score_comp, ties)   
 rps()

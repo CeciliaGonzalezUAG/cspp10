e04fe6471d2fe320d0613e734da50cb3c0c95742
Cecilia Gonzalez <ceciliagonzalez@uagateway.org>
2016-12-16 17:11:04 +0000

    Work

--- unit4/cgonzalez_craps.py
+++ unit4/cgonzalez_craps.py
@@ -11,22 +11,21 @@ import random
 #bet_amount
 #the current amount
 my_total=100
-
-def bet_amount():
-    bet=int(input("Place a bet under your current total:"))
+bet=int(input("Place a bet under your current total:"))
+def bet_amount(bet):
     if bet>my_total:
         print ("Please place affordable bet")
     return bet;
-    
-def get_rounds():
-    rounds=int(input("How many rounds would you like to play?: "))
+rounds=int(input("How many rounds would you like to play?: "))
+def get_rounds(rounds):
     return rounds;
 
-def roll_dice():
-    dice1=random.randint(1,6)
-    dice2=random.randint(1,6)
+dice1=random.randint(1,6)
+dice2=random.randint(1,6)
+    
+def roll_dice(dice1,dice2):
     roll1=dice1+dice2
-    return roll1;
+    print roll1;
     
 def game_winner(roll,my_total,bet):
     if roll==7 or roll==11:
@@ -37,9 +36,23 @@ def game_winner(roll,my_total,bet):
         print("You Lose!")
     else:
         point_number=roll
-        dice3=randomt-
+        dice3=random.randint(1,6)
+        dice4=random.randint(1,6)
+        roll2=dice3+dice4
+        return roll2;
 
-def  point_round(point_number):
+def  point_round(point_number,roll2,my_total,bet):
+    for point_number in range(point_number):
+        if roll2==point_number:
+            my_total=my_total+bet
+            print("You win")
+        elif roll2==7:
+            my_total=my_total-bet
+            print("You lose")
+        else:
+            return point_number;
+        
+        
     
     
 #to roll out 2 numbers to know whats the amount, set point, loss, or win

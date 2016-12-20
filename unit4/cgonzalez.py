1d1557435e6c1f2d0237ad51dc0d6147c6cdf586
Cecilia Gonzalez <ceciliagonzalez@uagateway.org>
2016-12-20 17:12:52 +0000

    Worked on lab, fixed functions

--- unit4/cgonzalez.py
+++ unit4/cgonzalez.py
@@ -1,15 +1,6 @@
 import random
 
-bank_account=100
-
-bet=int(input("Place a bet that you have enough for: "))
-
-dice1=random.randint(1,6)
-dice2=random.randint(1,6)
-roll1=dice1+dice2
-print("Your roll is: "+ str(roll1))
-
-def phase_one(roll1):
+def phase_one(roll1,point_number):
     if roll1==7 or roll1==11:
         bank_account=bet+bank_account
         print("You win!")
@@ -19,11 +10,36 @@ def phase_one(roll1):
         print("You Lose!")
         return "lost"
     else:
-        dice3=random.randint(1,6)
-        dice4=random.randint(1,6)
-        roll2=dice3+dice4
-        print("Going to Phase 2")
-        return roll2;
+        point_number=roll1
+        return roll1
+
+def point_round(point_number,roll2, bank_account,bet):
+    while roll2 != 7 :
+        if roll2== point_number:
+            bank_account=bank_account+bet
+            print("you win")
+
+def roll():
+    dice1=random.randint(1,6)
+    dice2=random.randint(1,6)
+    roll1=dice1+dice2
+    print("Your roll is: "+ str(roll1))
+    return roll1
+    
+bank_account=100
+
+while bank_account != 0:
+
+    bet=int(input("Place a bet that you have enough for: "))
+
+    roll1=roll()
     
     
+def craps():
+    score_player=0
+    score_house=0
+    while point_number != 7:
     
+
+        
+print(bank_account)

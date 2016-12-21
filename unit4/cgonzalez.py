2871bbde85b75c9a553c13142118abb7bec7b905
Cecilia Gonzalez <ceciliagonzalez@uagateway.org>
2016-12-21 17:14:04 +0000

    Continued on Lab

--- unit4/cgonzalez.py
+++ unit4/cgonzalez.py
@@ -1,23 +1,5 @@
 import random
 
-def phase_one(roll1,point_number):
-    if roll1==7 or roll1==11:
-        bank_account=bet+bank_account
-        print("You win!")
-        return "win"
-    elif roll1==2 or roll1==3 or roll1==12:
-        bank_account=bank_account-bet
-        print("You Lose!")
-        return "lost"
-    else:
-        point_number=roll1
-        return roll1
-
-def point_round(point_number,roll2, bank_account,bet):
-    while roll2 != 7 :
-        if roll2== point_number:
-            bank_account=bank_account+bet
-            print("you win")
 
 def roll():
     dice1=random.randint(1,6)
@@ -26,20 +8,54 @@ def roll():
     print("Your roll is: "+ str(roll1))
     return roll1
     
-bank_account=100
+def phase_one(roll1,point_number,bet,bank_account):
+    if roll1==7 or roll1==11:
+        bank_account=bet+bank_account
+        print("You win!")
+        return "You win"
+    elif roll1==2 or roll1==3 or roll1==12:
+        bank_account=bank_account-bet
+        print("You Lose!")
+        return "you lose"
+    else:
+        point_number=roll1
+        return roll1
 
-while bank_account != 0:
 
-    bet=int(input("Place a bet that you have enough for: "))
-
-    roll1=roll()
+def roll2():
+    dice3=random.randint(1,6)
+    dice4=random.randint(1,6)
+    roll2=dice3+dice4
+    print("Your roll is: "+ str(roll2))
+    return roll2
     
     
+def point_round(point_number,roll2, bank_account,bet,point):
+    while roll2 != 7 :
+        if roll2== point_number:
+            bank_account=bank_account+bet
+            print("you win")
+            return "you win"
+        elif roll2==7:
+            bank_account=bank_account-bet
+            return "House wins"
+            
+#point_round, phase_one, 
+
 def craps():
-    score_player=0
-    score_house=0
-    while point_number != 7:
-    
-
-        
-print(bank_account)
+    bank_account=100
+    bet=int(input("Place a bet that you have enough for: "))
+    roll1=roll()
+    while  bank_account != 0:
+        if bank_account ==0:
+            print("No money left, House won")
+        elif point_round=="you win":
+            print("You win!")
+        elif point_round=="House wins":
+            print("House wins")
+        elif phase_one=="You win":
+            print("You win!")
+        elif phase_one=="you lose":
+            print("House wins!")
+    print(bank_account)
+craps()
